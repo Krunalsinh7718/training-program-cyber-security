f27861821de2fbf2f1b882fbddb193e3faf4d363
Krunalsinh Vaghela <krunalsinh.vaghela@corpnet.co.in>
2024-01-24 11:53:01 +0530

    design changes

---
+++ js/pagejs/page-common.js
@@ -0,0 +1,38 @@
+//All elements that have a data-feather attribute will be replaced with SVG markup
+feather.replace();
+
+$(document).ready(function () {
+    
+
+   
+
+    //responsive navmenu revil button
+    $(document).on('click', '#navMenuRevilBtn', function () {
+        navMenuToggle();
+    })
+
+    //responsive navmenu close button
+    $(document).on('click', '#navMenuCloseBtn', function () {
+        navMenuToggle();
+    })
+
+    //common date picker
+    $('.custDatePicker').datetimepicker({
+        format: 'MM/DD/YYYY'
+    });
+    
+    $('.custTimePicker').datetimepicker({
+        format: 'hh:mm A'
+    })
+
+    //headroom
+    $("#mainHeader").headroom();
+    
+})
+
+
+//function for nav menu revil toggle
+const navMenuToggle = () => {
+    $("#pageNavLinkMenu").toggleClass("menu-active");
+    $(document).toggleClass("nav-menu-overflow-hidden");
+}
