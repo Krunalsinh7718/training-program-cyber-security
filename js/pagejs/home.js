78de4be33bbddc492f0b95380c70e477e17b1457
Krunalsinh Vaghela <krunalsinh.vaghela@corpnet.co.in>
2024-02-20 16:24:33 +0530

    design changes

--- js/pagejs/home.js
+++ js/pagejs/home.js
@@ -1,24 +1,40 @@
 $(document).ready(function(){
      //main banner slider
      var mainBannerSlider = $('#mainBannerSlider').owlCarousel({
-        loop: true,
+        // loop: true,
         nav: false,
         dots: false,
         items: 1,
         animateIn: 'fadeIn',
         animateOut: 'fadeOut',
-        autoplay: true,
+        // autoplay: true,
+        touchDrag  : false,
+        mouseDrag  : false
       });
 
       //main banner custom nav buttons
-      $('.web-main-banner .nav-prev').click(function () {
-        mainBannerSlider.trigger('prev.owl.carousel');
-      })
-      $('.web-main-banner .nav-next').click(function () {
-        console.log("next click");
-        mainBannerSlider.trigger('next.owl.carousel');
-      })
+      // $('.web-main-banner .nav-prev').click(function () {
+      //   mainBannerSlider.trigger('prev.owl.carousel');
+      // })
+      // $('.web-main-banner .nav-next').click(function () {
+      //   console.log("next click");
+      //   mainBannerSlider.trigger('next.owl.carousel');
+      // })
+      var newsFlashSlider = $('#news-flash-slider').owlCarousel({
+        // loop: true,
+        nav: false,
+        dots: false,
+        items: 1,
+        // animateIn: 'fadeIn',
+        // animateOut: 'fadeOut',
+        autoplay: true,
+        // touchDrag  : false,
+        // mouseDrag  : false
+      });
+
       
+      
+    
       //scroll to main content
       $("#scrollToMainContent").click(function () {
         $("html, body").animate({
